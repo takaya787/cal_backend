34d59cfbe9c069e5247b5953d6bb7cc87a9715ad
takaya787 <takaya318318@gmail.com>
2021-02-13 19:15:19 +0900

    jwtによるlogin認証 token作成　完了

---
+++ app/controllers/api/auth_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  class AuthController < ApplicationController
+    # before_action :authorized, only: [:auto_login]
+    def login
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        payload = {user_id: @user.id}
+        token = encode_token(payload)
+
+        render json: {user: @user, token: token}
+      else
+        render json: {error: 'Invalid username or password'}, status: :unauthorized
+      end
+    end
+
+    #Oauth認証とかで使用する場合のみ
+    # def auto_login
+    #   render json: {user: {email: @current_user.email, id: @current_user.id,name: @current_user.name}}
+    # end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace 'api' do
     resources :users
+
+    #login認証用
+    post '/login', to:"auth#login"
+    get '/auto_login', to:"auth#auto_login"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
