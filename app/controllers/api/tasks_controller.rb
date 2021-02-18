974d99bbb7bb9945f5b2d72b209e8051b4754815
takaya787 <takaya318318@gmail.com>
2021-02-18 11:25:18 +0900

    userによるtask作成　完了

---
+++ app/controllers/api/tasks_controller.rb
@@ -0,0 +1,71 @@
+module Api
+  class TasksController < ApplicationController
+    before_action :set_task, only: %i[ show update destroy ]
+    before_action :authorized, except: [:all]
+
+    # GET /tasks
+    # GET /tasks.json
+    def index
+      @tasks = Task.where(user_id: @current_user.id)
+      render json: @tasks, status: :ok
+    end
+
+    #シンプルに全てのtasksをloginなしで確認する用
+    # GET /tasks/all
+    def all
+      @tasks = Task.all
+      render json: @tasks
+    end
+
+    # GET /tasks/1
+    # GET /tasks/1.json
+    def show
+      render json:@task, status: :ok
+    end
+
+    # POST /tasks
+    # POST /tasks.json
+    def create
+      @task = @current_user.tasks.new(task_params)
+      #task チェック用のコード
+      # if @task.valid?
+      #   render json: @task, status: :ok
+      # else
+      #   render json: {event: @task, errors: @task.errors }, status: :bad_request
+      # end
+
+      if @task.save
+        render json: @task, status: :created, location: api_task_url(@task)
+      else
+        render json: {errors:@task.errors }, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /tasks/1
+    # PATCH/PUT /tasks/1.json
+    def update
+      if@task.update(user_params)
+        render :show, status: :ok, location: api_task_url(@task)
+      else
+        render json:@task.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /tasks/1
+    # DELETE /tasks/1.json
+    def destroy
+     @task.destroy
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_task
+        @task = Task.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def task_params
+        params.require(:task).permit(:title, :memo, :year, :month, :date)
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
+
   namespace 'api' do
 
     #シンプルに全てのeventsをloginなしで確認する用
     get '/events/all', to:"events#all"
+
+    #シンプルに全てのtasksをloginなしで確認する用
+    get '/tasks/all', to:"tasks#all"
+
     #login認証用
     post '/login', to:"auth#login"
     get '/auto_login', to:"auth#auto_login"
 
     #resources一覧
+    resources :tasks
     resources :users
     resources :events
   end
